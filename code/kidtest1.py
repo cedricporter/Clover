6f5d4419e74e482f71fd658ebba94519d9d68ded
kid <963262214@qq.com>
2012-01-27 22:27:58 +0800

    KidTest

---
+++ code/kidtest1.py
@@ -0,0 +1,14 @@
+import ogre.renderer.OGRE as ogre
+import SampleFramework as sf
+
+class KidTest1(sf.Application):
+
+    def _createScene(self):
+        pass
+
+def main():
+    ta = KidTest1()
+    ta.go()
+
+if __name__ == '__main__':
+    main()
