40cb2527692fce729a7512c2e6b85966d7462546
Cedric Porter <cedricporter@gmail.com>
2012-01-28 20:53:01 +0800

    把me改成了self。

--- code/main.py
+++ code/main.py
@@ -6,26 +6,26 @@ import SampleFramework as sf
 class tri(ogre.ManualObject): 
     #simplest possible manual object, for testing purposes 
     #takes 3 tuples A, B, C 
-    def __init__(me, name, A, B, C): 
-        me.A = A 
-        me.B = B 
-        me.C = C 
-        ogre.ManualObject.__init__(me, name) 
-        A, B, C = me.A, me.B, me.C 
-        me.clear() 
-        me.begin("default", ogre.RenderOperation.OT_TRIANGLE_STRIP) 
-        me.position(A[0], A[1], A[2]) 
-        me.normal(0, 0, 1) 
-        me.position(B[0], B[1], B[2]) 
-        me.position(C[0], C[1], C[2]) 
-        me.position(A[0], A[1], A[2]) 
-        me.end() 
+    def __init__(self, name, A, B, C): 
+        self.A = A 
+        self.B = B 
+        self.C = C 
+        ogre.ManualObject.__init__(self, name) 
+        A, B, C = self.A, self.B, self.C 
+        self.clear() 
+        self.begin("default", ogre.RenderOperation.OT_TRIANGLE_STRIP) 
+        self.position(A[0], A[1], A[2]) 
+        self.normal(0, 0, 1) 
+        self.position(B[0], B[1], B[2]) 
+        self.position(C[0], C[1], C[2]) 
+        self.position(A[0], A[1], A[2]) 
+        self.end() 
 
 class TutorialApplication(sf.Application): 
 
     def _createScene(self): 
         sceneManager = self.sceneManager 
-        sceneManager.ambientLight = ogre.ColourValue (0.3,0.3,0.3) 
+        sceneManager.ambientLight = (0.3,0.3,0.3) 
         self.ent = tri("triangle", (0,0,0), (0,100,0), (100,0,0) ) 
         node1 = sceneManager.getRootSceneNode().createChildSceneNode ("node1") 
         node1.attachObject (self.ent) 
