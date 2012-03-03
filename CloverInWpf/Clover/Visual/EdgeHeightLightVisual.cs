d409c2822fd274fb9685e3240761d59dae47ef08
kid <963262214@qq.com>
2012-03-03 10:54:53 +0800

    边的视觉效果失败了- -

--- CloverInWpf/Clover/Visual/EdgeHeightLightVisual.cs
+++ CloverInWpf/Clover/Visual/EdgeHeightLightVisual.cs
@@ -4,23 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Windows.Shapes;
 using System.Windows.Media;
-using System.Windows.Controls;
+using System.Windows;
 
 namespace Clover.Visual
 {
-    class VertexHeightLightVisual : VisualElementFactory
+    class EdgeHeightLightVisual : VisualElementFactory
     {
-        Ellipse mark;
 
-        public VertexHeightLightVisual(Brush brush, Double posX, Double posY)
+        Rectangle mark;
+
+        public EdgeHeightLightVisual(Brush brush, Point p1, Point p2)
         {
-            mark = new Ellipse();
-            mark.Height = mark.Width = 10;
-            mark.Margin = new System.Windows.Thickness(0, -5, 0, 0);
+            Vector v = p2 - p1;
+            double deg = Vector.AngleBetween(v, new Vector(0, -1));
+            mark = new Rectangle();
+            mark.Width = 10;
+            mark.Height = v.Length;
+            mark.RadiusX = mark.RadiusY = 2;
+            //mark.Margin = new System.Windows.Thickness(0, -5, 0, 0);
             mark.Fill = brush;
             box.Children.Add(mark);
             box.Opacity = 0;
-            TransformGroup = new TranslateTransform(posX, posY);
+            TransformGroup tg = new TransformGroup();
+            TranslateTransform ts = new TranslateTransform(p1.X, p1.Y);
+            RotateTransform tr = new RotateTransform(Vector.AngleBetween(v, new Vector(0, -1)));
+            tg.Children.Add(tr);
+            tg.Children.Add(ts);
+            
+            TransformGroup = tg;
             //mark.Opacity = 0;
         }
 
@@ -36,7 +47,7 @@ namespace Clover.Visual
 
         public override void Display()
         {
-            
+
         }
 
         public override void FadeOut()
