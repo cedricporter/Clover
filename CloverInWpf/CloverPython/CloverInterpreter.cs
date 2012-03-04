7417ee5383fa094d83ca08d1b3605e1690d4df04
Cedric Porter <cedricporter@gmail.com>
2012-03-04 18:46:50 +0800

    嵌入python

---
+++ CloverInWpf/CloverPython/CloverInterpreter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+namespace CloverPython
+{
+    public class CloverInterpreter
+    {
+        ScriptEngine pythonEngine = null;
+        ScriptScope pythonScope = null;
+
+        /// <summary>
+        /// 初始化解释器
+        /// </summary>
+        public void InitialzeInterpreter()
+        {
+            pythonEngine = Python.CreateEngine();
+            pythonScope = pythonEngine.CreateScope();
+        }
+
+
+
+
+    }
+}
