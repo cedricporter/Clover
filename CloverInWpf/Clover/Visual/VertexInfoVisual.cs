c352707bfe7bd506c74a79e002d45a7bb035cdd8
Cedric Porter <cedricporter@gmail.com>
2012-03-06 03:18:22 +0800

    半透明Info

--- CloverInWpf/Clover/Visual/VertexInfoVisual.cs
+++ CloverInWpf/Clover/Visual/VertexInfoVisual.cs
@@ -19,6 +19,7 @@ namespace Clover.Visual
             vertex = v;
             infotext.Foreground = new SolidColorBrush(Colors.Black);
             infotext.Background = new SolidColorBrush(Colors.White);
+            infotext.Opacity = 0.5;
             box.Children.Add(infotext);
             UpdateInfoCallBack(v, null);
         }
