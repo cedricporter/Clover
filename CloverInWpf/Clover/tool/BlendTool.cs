c12a0ecd1f15f8b57857fc47ddf5d9f88def4671
kid <963262214@qq.com>
2012-03-10 13:14:15 +0800

    将FoldTool关于Blending的代码都注释掉了

---
+++ CloverInWpf/Clover/tool/BlendTool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/**
+@date		:	2012/03/03
+@filename	: 	MaterialController.cs
+@author		:	屠文翔	
+			 -     _     - 
+			| | - (_) __| |
+			| |/ /| |/ _  |
+			|   < | |||_| |
+			|_|\_\|_|\____|
+@note		:	Blending工具
+**/
+
+namespace Clover.Tool
+{
+    class BlendTool
+    {
+    }
+}
